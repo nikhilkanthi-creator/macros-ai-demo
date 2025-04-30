110307eaf770e88d04c36e7ca26a5667c4e65363
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-30 00:31:38 +0000

    finite incantatem

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -7,25 +7,58 @@ class MacrosController < ApplicationController
   def process_inputs
     @the_description = params.fetch("description_area")
     @the_image = params.fetch("image")
-
-    request_headers_hash = {
-      "Authorization" => "Bearer #{ENV.fetch("OPENAI_API_KEY")}", "content-type" => "application/json"
-    }
-
-    request_body_hash = {
-       "model" => "gpt-3.6-turbo", "messages" => [{
-         "role" => "system"
-         "content" => "You are a dietician looking at an image and telling me how much calories it has."
-       },
-       {
-        "role" => "user", "content" => ""
-       }
-      ]
-     }
+    @the_image_converted = DataURI.convert(@the_image)
+    
+    c = OpenAI::Chat.new
+    c.system("You are an expert nutritionist. Estimate the macronutrients (carbohydrates, protein, and fat) in grams, as well as total calories in kCal.")
+    c.user(@the_description, image: @the_image)
+    c.schema = '{
+  "name": "my_schema",
+  "schema": {
+    "type": "object",
+    "properties": {
+      "carbohydrates": {
+        "type": "number",
+        "description": "The amount of carbohydrates in grams."
+      },
+      "protein": {
+        "type": "number",
+        "description": "The amount of protein in grams."
+      },
+      "fats": {
+        "type": "number",
+        "description": "The amount of fats in grams."
+      },
+      "total_calories": {
+        "type": "number",
+        "description": "The total calorie content in kilocalories."
+      },
+      "free_text": {
+        "type": "string",
+        "description": "A summarization or description of the food and its macronutrients."
+      }
+    },
+    "required": [
+      "carbohydrates",
+      "protein",
+      "fats",
+      "total_calories",
+      "free_text"
+    ],
+    "additionalProperties": false
+      },
+      "strict": true
+    }'
+    
+    @answer = c.assistant!
+    @carbohydrates = @answer.fetch("carbohydrates")
+    @protein = @answer.fetch("protein")
+    @fats = @answer.fetch("fats")
+    @total_calories = @answer.fetch("total_calories")
+    @free_text = @answer.fetch("free_text")
 
     render({ :template => "macro_template/results"})
 
-    
   end
 
 end
